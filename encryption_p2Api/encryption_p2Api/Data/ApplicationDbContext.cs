932b41626cef7761cb21ef5c978d8a1d5d91f7a3
Nahuubj <nahuel.gutierrez.vargas17@gmail.com>
2025-03-16 18:05:26 -0400

    API
    Configura ConnectionString

---
+++ encryption_p2Api/encryption_p2Api/Data/ApplicationDbContext.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace encryption_p2Api
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Asistance> Asistances { get; set; }
+        public DbSet<Log> Logs { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Employee>()
+                .Property(e => e.Code)
+                .HasMaxLength(10);
+
+            modelBuilder.Entity<Employee>()
+                .Property(e => e.FullName)
+                .HasMaxLength(50);
+
+            modelBuilder.Entity<Employee>()
+                .Property(e => e.UpdateDate)
+                .HasMaxLength(10);
+
+            modelBuilder.Entity<Employee>()
+                .Property(e => e.Department)
+                .HasMaxLength(50);
+
+            modelBuilder.Entity<Asistance>()
+                .Property(a => a.Type)
+                .HasMaxLength(40);
+
+            modelBuilder.Entity<Log>()
+                .Property(l => l.Action)
+                .HasMaxLength(50);
+        }
+    }
+}
---
+++ encryption_p2Api/encryption_p2Api/Log.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace encryption_p2Api
+{
+    public class Log
+    {
+        [Column("id")]
+        public int ID { get; set; }
+        [Column("employeeID")]
+        public int employeeID { get; set; }
+        [Column("action"), StringLength(50, ErrorMessage = "Maximo 50 caracteres")]
+        public string Action { get; set; }
+        [Column("registrationDate")]
+        public DateTime RegistrationDate { get; set; }
+    }
+}
